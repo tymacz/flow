4edf89b6cfbdfcba10bba49b0b96b2c8f4a03bf4
tymacz <maxence.rebours@outlook.fr>
2026-01-04 21:11:32 +0100

    Add splash and home page with native splash support
    - Created a new SplashPage that displays a logo and navigates to the HomePage after a 3-second delay.
    - Implemented a HomePage with a simple welcome message.
    - Added launch background image assets for the iOS application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,168 @@
+import 'package:flow/home_page.dart';
+import 'package:flow/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    List<Widget> _buildScreens() {
+        return [
+          HomePage(),
+          screen2(),
+          screen3(),
+          screen4(),
+        ];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+        return [
+            PersistentBottomNavBarItem(
+                icon: Icon(Icons.home),
+                inactiveIcon: Icon(Icons.home_outlined),
+                title: ("Accueil"),
+                activeColorPrimary: Color(0xFF6366F1),
+                inactiveColorPrimary: Color.fromARGB(255, 108, 122, 141),
+            ),
+            PersistentBottomNavBarItem(
+                icon: Icon(Icons.fitness_center),
+                inactiveIcon: Icon(Icons.fitness_center_outlined),
+                title: ("Exercices"),
+                activeColorPrimary: Color(0xFF6366F1),
+                inactiveColorPrimary: Color.fromARGB(255, 108, 122, 141),
+            ),
+            PersistentBottomNavBarItem(
+                icon: Icon(Icons.analytics),
+                inactiveIcon: Icon(Icons.analytics_outlined),
+                title: ("Progression"),
+                activeColorPrimary: Color(0xFF6366F1),
+                inactiveColorPrimary: Color.fromARGB(255, 108, 122, 141),
+            ),
+            PersistentBottomNavBarItem(
+                icon: Icon(Icons.person),
+                inactiveIcon: Icon(Icons.person_outline),
+                title: ("Profil"),
+                activeColorPrimary: Color(0xFF6366F1),
+                inactiveColorPrimary: Color.fromARGB(255, 108, 122, 141),
+            ),
+        ];
+    }
+    PersistentTabController _controller;
+
+_controller = PersistentTabController(initialIndex: 0);
+    return  PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        handleAndroidBackButtonPress: true, 
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        hideNavigationBarWhenKeyboardAppears: true,
+        padding: const EdgeInsets.only(top: 8),
+        backgroundColor: Color(0xFFFFFFFF),
+        isVisible: true,
+        animationSettings: const NavBarAnimationSettings(
+            navBarItemAnimation: ItemAnimationSettings(
+                duration: Duration(milliseconds: 400),
+                curve: Curves.ease,
+            ),
+            screenTransitionAnimation: ScreenTransitionAnimationSettings( 
+                animateTabTransition: true,
+                duration: Duration(milliseconds: 200),
+                screenTransitionAnimationType: ScreenTransitionAnimationType.fadeIn,
+            ),
+        ),
+        confineToSafeArea: true,
+        navBarHeight: kBottomNavigationBarHeight,
+        navBarStyle: NavBarStyle.style3,
+      );
+  }
+}
+
+class screen1 extends StatelessWidget {
+  const screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Screen 1'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: const Center(
+        child: Text('This is Screen 1'),
       ),
     );
   }
 }
+
+class screen2 extends StatelessWidget {
+  const screen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen 2'),
+      ),
+      body: const Center(
+        child: Text('This is Screen 2'),
+      ),
+    );
+  }
+}
+
+class screen3 extends StatelessWidget {
+  const screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen 3'),
+      ),
+      body: const Center(
+        child: Text('This is Screen 3'),
+      ),
+    );
+  }
+}
+
+class screen4 extends StatelessWidget {
+  const screen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Screen 4'),
+      ),
+      body: const Center(
+        child: Text('This is Screen 4'),
+      ),
+    );
+  }
+}
+
+
+
