e038ea50a12d9787f8166423d928b1ea916f92ce
tymacz <75813559+tymacz@users.noreply.github.com>
2026-08-24 16:37:52 +0200

    Implement async rewrites in next.config.js
    Add async rewrites for API calls to backend

--- flow-web/next.config.js
+++ flow-web/next.config.js
@@ -7,8 +7,14 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
+  async rewrites() {
+    return [
+      {
+        // Tout appel vers /api/... côté client sera redirigé vers le backend Docker
+        source: '/api/:path*',
+        destination: 'http://backend:8000/api/:path*',
+      },
+    ];
   },
 };
 
