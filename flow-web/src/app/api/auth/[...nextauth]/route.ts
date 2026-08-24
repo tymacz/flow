98648aa723a415523339089451a9cdef5cef4f5a
tymacz <75813559+tymacz@users.noreply.github.com>
2026-08-24 17:19:47 +0200

    Refactor NextAuth handler export for GET and POST

--- flow-web/src/app/api/auth/[...nextauth]/route.ts
+++ flow-web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { handlers } from "@/server/auth";
 
-export const { GET, POST } = handlers;
-
 export const authOptions = {
   trustHost:true,};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
