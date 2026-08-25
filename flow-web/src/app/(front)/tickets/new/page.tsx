048b5c776910f6bc81fb66eaffda2fe186e58a45
tymacz <75813559+tymacz@users.noreply.github.com>
2026-08-25 12:38:48 +0200

    Add NewTicketPage component with TicketForm

---
+++ flow-web/src/app/(front)/tickets/new/page.tsx
@@ -0,0 +1,9 @@
+import TicketForm from '@/components/TicketForm';
+
+export default function NewTicketPage() {
+  return (
+    <main className="container mx-auto py-12 px-4">
+      <TicketForm />
+    </main>
+  );
+}
